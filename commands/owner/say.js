f96393a082c07e1f493609b6326995ec681866b9
HyperSoWeak <hypersoweak@gmail.com>
2024-01-28 01:25:23 +0800

    feat: add command "say"

---
+++ commands/owner/say.js
@@ -0,0 +1,19 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+
+module.exports = {
+	ownerOnly: true,
+	cooldown: 0,
+	data: new SlashCommandBuilder()
+		.setName('say')
+		.setDescription('Say anything as the bot!')
+		.setDefaultMemberPermissions('0')
+		.addStringOption(option =>
+			option.setName('message')
+				.setDescription('The message to say')
+				.setRequired(true)),
+	async execute(interaction) {
+		const message = interaction.options.getString('message');
+		await interaction.channel.send(message);
+		return interaction.reply({ content: 'Success!', ephemeral: true });
+	},
+};
